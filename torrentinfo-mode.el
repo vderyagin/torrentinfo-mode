968507e6bce27752e7092a9c65b3f9467fa43529
Victor Deryagin <vderyagin@gmail.com>
2015-04-10 22:42:59 +0300

    Bunch of small fixes and modifications.

--- torrentinfo-mode.el
+++ torrentinfo-mode.el
@@ -32,20 +32,13 @@
 
 (defun torrentinfo--cmd (file)
   "Command line to get information about FILE."
-  (let ((flag (cond
-               ((eq torrentinfo-detail-level 'files) "--files")
-               ((eq torrentinfo-detail-level 'everything) "--everything")
-               ((eq torrentinfo-detail-level 'minimal) "")
-               (t (error "invalid detail level: %s" (symbol-name torrentinfo-detail-level)))))
-        (file (shell-quote-argument file)))
-    (format "torrentinfo %s %s" flag file)))
-
-(defun torrentinfo-file-handler (_ file &rest __)
-  (unless (executable-find "torrentinfo")
-    (error "torrentinfo(1) appears to not be installed"))
-  (setq buffer-file-name (expand-file-name file))
-  (insert (torrentinfo--get-info file))
-  (goto-char (point-min)))
+  (format "torrentinfo %s %s"
+          (pcase torrentinfo-detail-level
+            (`files "--files")
+            (`everything "--everything")
+            (`minimal "")
+            (_ (error "invalid detail level: %s" torrentinfo-detail-level)))
+          (shell-quote-argument file)))
 
 (defun torrentinfo--get-info (file)
   (ansi-color-apply (shell-command-to-string (torrentinfo--cmd file))))
@@ -68,26 +61,36 @@
   (erase-buffer)
   (insert (torrentinfo--get-info buffer-file-name))
   (goto-char (point-min))
-  (read-only-mode)
+  (read-only-mode 1)
   (set-buffer-modified-p nil))
 
-(put 'torrentinfo-file-handler 'safe-magic t)
-(put 'torrentinfo-file-handler 'operations '(insert-file-contents))
-(add-to-list 'file-name-handler-alist '("\\.torrent\\'" . torrentinfo-file-handler))
+;;;###autoload
+(defun torrentinfo-file-handler (_ file &rest __)
+  (unless (executable-find "torrentinfo")
+    (error "torrentinfo(1) appears to not be installed"))
+  (setq buffer-file-name (expand-file-name file))
+  (insert (torrentinfo--get-info file))
+  (goto-char (point-min)))
 
 ;;;###autoload
 (define-derived-mode torrentinfo-mode fundamental-mode "TorrentInfo"
-  "Major mode for viewing information about torrent files.")
-
-(add-hook 'torrentinfo-mode-hook 'hl-line-mode)
+  "Major mode for viewing information about torrent files."
+  (read-only-mode 1)
+  (hl-line-mode))
 
 (define-key torrentinfo-mode-map (kbd "p") 'previous-line)
 (define-key torrentinfo-mode-map (kbd "n") 'next-line)
+(define-key torrentinfo-mode-map (kbd "SPC") 'scroll-up-command)
+(define-key torrentinfo-mode-map (kbd "S-SPC") 'scroll-down-command)
 (define-key torrentinfo-mode-map (kbd "q") (lambda () (interactive) (kill-buffer (current-buffer))))
 (define-key torrentinfo-mode-map (kbd "<tab>") 'torrentinfo-change-detail-level)
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.torrent\\'" . torrentinfo-mode))
+(progn
+  (put 'torrentinfo-file-handler 'safe-magic t)
+  (put 'torrentinfo-file-handler 'operations '(insert-file-contents))
+  (add-to-list 'file-name-handler-alist '("\\.torrent\\'" . torrentinfo-file-handler))
+  (add-to-list 'auto-mode-alist '("\\.torrent\\'" . torrentinfo-mode)))
 
 (provide 'torrentinfo-mode)
 
